e7ab299eb4cd7c35517b53e95d312da5e1e0ed94
zhaiyu <zhaiyu_hb@hotmail.com>
2019-12-29 16:05:20 +0800

    main.py

--- main.py
+++ main.py
@@ -7,14 +7,16 @@ content_last = '100224'  # 商家固定值结尾 100224
 business_no = 'TJMP1F101'  # 商铺号 TJMP1F101
 
 app = xw.App(visible=False, add_book=False)
-app.display_alerts=False
-app.screen_updating= False
-wb = app.books.open(r'./files/20191218145136.xlsx') # 把需要转成txt的原表格文件放到files文件夹下
-list_value = wb.sheets[0].range('A6', 'F100').value  # 需要导出的原表格文件内容范围；参数 range(表格起始位置，F100表格截止位置)
+app.display_alerts = False
+app.screen_updating = False
+wb = app.books.open(r'./files/123456.xlsx') # 把需要转成txt的原表格文件放到files文件夹下
+list_value = wb.sheets[0].range('A6', 'F300').value  # 需要导出的原表格文件内容范围；参数 range(表格起始位置，F100表格截止位置)
 
 txt_file = './files/' + business_no + '_' + time.strftime('%Y%m%d%H%M%S', time.localtime(time.time())) + '_LIST.txt'  # 需要生成的txt文件，生成在files文件夹下
 f = open(txt_file, 'w')
 for i in range(len(list_value)):
+    if list_value[i][0] == None :
+        continue
     # 第1列
     f.write(content_first)
     f.write(',')
